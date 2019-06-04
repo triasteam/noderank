9a564720d8d4605f26b8c0c20cd15d66c08762b2
jdyuankai <yuank1987@163.com>
2019-06-04 16:24:13 +0800

    [fix #82]noderank returns empty string

--- noderank_test.go
+++ noderank_test.go
@@ -9,26 +9,26 @@ import (
 func TestGetRank(t *testing.T) {
 
 	info := []string{"A", "B", "1"}
-	noderank.AddAttestationInfo(info)
+	noderank.AddAttestationInfo("","",info)
 	info = []string{"B", "C", "1"}
-	noderank.AddAttestationInfo(info)
+	noderank.AddAttestationInfo("","",info)
 	info = []string{"C", "D", "1"}
-	noderank.AddAttestationInfo(info)
+	noderank.AddAttestationInfo("","",info)
 	info = []string{"D", "A", "1"}
-	noderank.AddAttestationInfo(info)
+	noderank.AddAttestationInfo("","",info)
 	info = []string{"A", "C", "1"}
-	noderank.AddAttestationInfo(info)
+	noderank.AddAttestationInfo("","",info)
 
-	actural := noderank.GetRank("1", 1)
+	_,actural,_ := noderank.GetRank("",1, 1)
 
 	if reflect.DeepEqual(len(actural), 1) != true {
 		t.Error("expected", 1, "but got ", len(actural))
 	}
 
 	acturalStr := actural[0].Attestee
-	expected := "C"
+	expected := "D"
 
-	if reflect.DeepEqual(acturalStr, "C") != true {
+	if reflect.DeepEqual(acturalStr, expected) != true {
 		t.Error("expected", expected, "but got ", acturalStr)
 	}
 }
